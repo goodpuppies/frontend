15a515d6ab15a899572dc40125a4adaccbec7998
mommysgoodpuppy <theelectronicfreezer@gmail.com>
2026-03-07 23:07:09 +0200

    dont strict

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,5 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root') as HTMLElement).render(
-  <StrictMode>
     <App />
-  </StrictMode>,
 )
